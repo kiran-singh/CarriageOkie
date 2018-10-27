1f04e346e9972f4769b65cbfaf94fa438d5d2dde
kiran-singh <kiran_cardiff@yahoo.co.uk>
2018-10-27 23:04:15 +0100

    Home page loading trains & carriages with songs with test data

--- CarriageOKie/Controllers/HomeController.cs
+++ CarriageOKie/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CarriageOKie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CarriageOKie.Controllers
 {
@@ -12,21 +10,62 @@ namespace CarriageOKie.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return View(new TrainCarriagesViewModel
+            {
+                Trains = new List<SelectListItem>
+                {
+                    new SelectListItem{Text = "Birmingham", Value = "1045"},
+                    new SelectListItem{Text = "Brighton", Value = "1732"},
+                    new SelectListItem{Text = "London", Value = "2798"},
+                    new SelectListItem{Text = "Leeds", Value = "4526"},
+                    new SelectListItem{Text = "Newcastle", Value = "5234"},
+                },
+                Carriages = new List<SelectListItem>
+                {
+                    new SelectListItem{Text = "A", Value = "A"},
+                    new SelectListItem{Text = "B", Value = "B"},
+                    new SelectListItem{Text = "C", Value = "C"},
+                },
+                GenreSongsViewModels = new List<GenreSongsViewModel>
+                {
+                    new GenreSongsViewModel
+                    {
+                        Genre = "Ballads",
+                        Songs = new List<string>
+                        {
+                            "Come See About Me",
+                            "I'll Never Love Again",
+                            "The End of Love",
+                            "Trip",
+                            "Who Hurt You?",
+                        }
+                    },
+                    new GenreSongsViewModel
+                    {
+                        Genre = "Rock",
+                        Songs = new List<string>
+                        {
+                            "Crawling",
+                            "Iris",
+                            "Papercut",
+                            "Somewhere I belong",
+                            "What I've done",
+                        }
+                    },
+                    new GenreSongsViewModel
+                    {
+                        Genre = "Techno",
+                        Songs = new List<string>
+                        {
+                            "ASCENTION",
+                            "ERUPTION",
+                            "DISRUPTION",
+                            "GUEDE (ARTBAT RAVE MIX)",
+                            "THE HATCHET",
+                        }
+                    }
+                }
+            });
         }
 
         public IActionResult Error()
